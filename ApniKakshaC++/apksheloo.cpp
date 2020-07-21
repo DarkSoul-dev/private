6dde2f028a68753f1e0c0814f7e0d36761ad8e83
DarkSoul-dev <lovekush098@privatemail.com>
2020-07-21 14:15:17 +0530

    Inital commit

---
+++ ApniKakshaC++/apksheloo.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(int argc, char const *argv[])
+{
+	int a=12,b=10;
+	float g=2.3,z;
+	double d=2.34509;
+	z=a+b*g;
+	cout<<"total"<<z<<endl;
+
+	return 0;
+}
---
+++ ApniKakshaC++/sushil.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	int a,b;
+	cout<<"heloo sushil kumar"<<endl;
+	cout<<"the pubg lover"<<endl;
+	cin>>a;
+	cin>>b;
+	if (a<b)
+{
+	cout<<"bolt action"<<endl;
+}
+	else
+{
+	cout<<"assualt rifle"<<endl;
+}
+	return 0;
+}
+
+
